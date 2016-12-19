f7b6b933dcd42926cc5b41dc03fea868d176f236
Pavel Anulyev <dev.anuljevpavel@gmail.com>
2016-12-19 18:35:31 +0300

    Revert "MainWindowFix"
    This reverts commit ada0bc95a8e2d63287807ac7c46b37e427d04714.

--- AnastasiaAssistant/MainWindow.xaml.cs
+++ AnastasiaAssistant/MainWindow.xaml.cs
@@ -115,9 +115,8 @@ namespace AnastasiaAssistant
         }
         private void FillCommandList()
         {
-            string path = Directory.GetCurrentDirectory();
             Encoding enc = Encoding.GetEncoding(1251);
-            StreamReader f = new StreamReader(path + @"\Commands.txt",enc);
+            StreamReader f = new StreamReader(System.Windows.Forms.Application.StartupPath + @"\Commands.txt",enc);
             string[] a = f.ReadToEnd().Split('\n');
             foreach (var K in a)
             {
@@ -640,12 +639,9 @@ namespace AnastasiaAssistant
             }
         }
 
-
         #endregion
 
-
-
-  
+      
     }
     public class ValueAngleConverter : IMultiValueConverter
     {
